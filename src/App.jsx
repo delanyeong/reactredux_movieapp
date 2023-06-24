1c2784d9dd1a356a1c2ce92217b71b6ea8dce584
delanyeong <delan.april@gmail.com>
2023-06-24 21:20:15 +0800

    create redux store and provide(provider) the redux store to react

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import {fetchDataFromApi} from "./utils/api";
 function App() {
 
   useEffect(() => {
-    apiTesting();
   }, []);
   
   const apiTesting = () => {
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
+import store from './app/store'
+import { Provider } from 'react-redux'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
     <App />
+  </Provider>
+
 )
