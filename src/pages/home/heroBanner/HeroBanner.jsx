4169813867949276416ee78eb91e854b96b5f462
delanyeong <delan.april@gmail.com>
2023-06-26 10:54:16 +0800

    finished herobanner tentatively

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,13 +1,27 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom"
-
+import { useSelector } from "react-redux"
 
 import "./style.scss"
+
+import useFetch from '../../../hooks/useFetch'
+
+import Img from "../../../components/lazyLoadImage/Img"
+import ContentWrapper from '../../../components/contentWrapper/contentWrapper'
 
 const HeroBanner = () => {
   const [background, setBackground] = useState("");
   const[query, setQuery] = useState("");
   const navigate = useNavigate();
+  const { url } = useSelector((state) => state.home)
+
+  //useFetch
+  const {data, loading} = useFetch("/movie/upcoming")
+
+  useEffect(()=> {
+    const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20 )].backdrop_path
+    setBackground(bg)
+  },[data])
 
   const searchQueryHandler = (event) => {
     // search query should not be empty and run search query only after entering
@@ -18,7 +32,17 @@ const HeroBanner = () => {
 
   return (
     <div className="heroBanner">
-      <div className="wrapper">
+
+      <div className="backdrop-img">
+        {!loading && <Img src={background}/>}
+      </div>
+
+      <div className="opacity-layer">
+        
+      </div>
+      <ContentWrapper>
+
+      
         <div className="heroBannerContent">
           <span className="title">Welcome</span>
           <span className="subTitle">Millions of movies, TV shows and people to discover. Explore now.</span>
@@ -30,7 +54,9 @@ const HeroBanner = () => {
             <button>Search</button>
           </div>
         </div>
-      </div>
+      
+      </ContentWrapper>
+
     </div>
   )
 }
